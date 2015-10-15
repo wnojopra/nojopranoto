33cecc5119038bd8fe093089b6546ab4b87eebb9
Willy Nojopranoto <willy.nojopranoto@oxygencloud.com>
2015-10-15 15:45:32 -0700

    First test of FannyFacts.

--- hello.py
+++ hello.py
@@ -1,8 +1,16 @@
+import random
 import os
 from flask import Flask
 
 app = Flask(__name__)
 
+FANNY_FACTS = [
+	'Did you know? The average Fanny survives off a diet of pistachios and milk tea.',
+	'Fanny is either on her period, or not on her period. There is a 50% chance Fanny is on her period.',
+	'The average Fanny hates you.',
+	'Fanny has 3-starred every puzzle in Hungry Cat Picross! Me-wow!'
+]
+
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return random.choice(FANNY_FACTS)
